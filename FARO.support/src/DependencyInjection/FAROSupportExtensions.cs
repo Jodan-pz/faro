0f7be3f3416bd31c2bc2ce991a84d19f0aae7123
Jd <jodan.pz@gmail.com>
2022-07-10 10:17:06 +0000

    dg

--- FARO.support/src/DependencyInjection/FAROSupportExtensions.cs
+++ FARO.support/src/DependencyInjection/FAROSupportExtensions.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using FARO.Common;
@@ -14,7 +14,7 @@ namespace FARO.Extensions.DependencyInjection {
 
         public static IServiceCollection AddFAROSupport(this IServiceCollection services,
                                                         IConfiguration configuration,
-                                                        IWebHostEnvironment? webHostEnvironment = null) {
+                                                        IHostEnvironment? webHostEnvironment = null) {
             // configure console logging
             services.AddLogging(lb => {
                 lb.AddConfiguration(configuration.GetSection("Logging"))
@@ -37,7 +37,7 @@ namespace FARO.Extensions.DependencyInjection {
             return serviceProvider;
         }
 
-        public static IApplicationBuilder UseFAROSupport(this IApplicationBuilder appBuilder, IConfiguration configuration, IWebHostEnvironment? webHostEnvironment = null) => appBuilder;
+        public static IApplicationBuilder UseFAROSupport(this IApplicationBuilder appBuilder, IConfiguration configuration, IHostEnvironment? webHostEnvironment = null) => appBuilder;
     }
 
 }
